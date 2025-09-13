b5bbe157dc24534dad5ad041a1266849f7960fa9
Johnkoderr <alexkool3008@gmail.com>
2025-09-14 02:55:49 +0800

    added eslint & prettier configuration

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,22 +1,22 @@
- const { merge } = require('webpack-merge');
- const common = require('./webpack.common.js');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
 
- module.exports = merge(common, {
-   mode: 'development',
-   devtool: 'inline-source-map',
-   module: {
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
     rules: [
       {
         test: /\.html$/i,
-        loader: "html-loader",
+        loader: 'html-loader',
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
-   devServer: {
-     static: './dist',
-   },
- });
+  devServer: {
+    static: './dist',
+  },
+});
